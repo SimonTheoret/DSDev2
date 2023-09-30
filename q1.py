e1b094c8aa97f33f4b484e1387a272ed0029a15e
Simon Theoret <simonteoret@hotmail.com>
2023-09-30 08:52:57 -0400

    Nettoyage et contains bugged!

--- q1.py
+++ q1.py
@@ -9,46 +9,28 @@ def count_labels(labels: str) -> int:
     Par exemple:
     "/m/04rlf,/m/06_fw,/m/09x0r" -> 3
     """
-<<<<<<< HEAD
+
     # TODO
-    pass
-=======
     return len(set(labels.split(",")))
->>>>>>> c4aeab0 ("init yeahbadou")
 
 
 def convert_id(ID: str) -> str:
     """
-<<<<<<< HEAD
-    Créez une fonction qui prend un ID d'étiquette (par exemple "/m/09x0r") et renvoie le nom d'étiquette correspondant (par exemple "Speech")
+    <<<<<<< HEAD
+        Créez une fonction qui prend un ID d'étiquette (par exemple "/m/09x0r") et renvoie le nom d'étiquette correspondant (par exemple "Speech")
 
-    Pour ce faire, utilisez la bibliothèque `json` et le fichier `data/ontology.json`, une description du fichier peut être trouvée
-    sur https://github.com/audioset/ontology
+        Pour ce faire, utilisez la bibliothèque `json` et le fichier `data/ontology.json`, une description du fichier peut être trouvée
+        sur https://github.com/audioset/ontology
 
-    Même si lire le fichier à chaque fois et parcourir les éléments pour trouver une correspondance fonctionne assez bien dans notres cas.
-    Pensez à des moyens d'accélérer ce processus si, par exemple, cette fonction devait être exécutée 100 000 fois.
+        Même si lire le fichier à chaque fois et parcourir les éléments pour trouver une correspondance fonctionne assez bien dans notres cas.
+        Pensez à des moyens d'accélérer ce processus si, par exemple, cette fonction devait être exécutée 100 000 fois.
     """
     # TODO
-    pass
-=======
-    Créez une fonction qui prend un ID d'étiquette (par exemple "/m/09x0r") et
-    renvoie le nom d'étiquette correspondant (par exemple "Speech")
-
-    Pour ce faire, utilisez la bibliothèque `json` et le fichier
-    `data/ontology.json`, une description du fichier peut être trouvée sur
-    https://github.com/audioset/ontology
-
-    Même si lire le fichier à chaque fois et parcourir les éléments pour trouver
-    une correspondance fonctionne assez bien dans notres cas.  Pensez à des
-    moyens d'accélérer ce processus si, par exemple, cette fonction devait être
-    exécutée 100 000 fois.
-    """
     with open("./data/ontology.json") as f:
         data_dict = json.load(f)
         for object in data_dict:
             if object["id"] == ID:
                 return object["name"]
->>>>>>> c4aeab0 ("init yeahbadou")
 
 
 def convert_ids(labels: str) -> str:
@@ -60,9 +42,6 @@ def convert_ids(labels: str) -> str:
     "/m/04rlf,/m/06_fw,/m/09x0r" -> "Musique|Skateboard|Discours"
     """
     # TODO
-<<<<<<< HEAD
-    pass
-=======
     splitted_labels: list[str] = labels.split(",")
     label_link = ""
     for label in splitted_labels:
@@ -71,12 +50,10 @@ def convert_ids(labels: str) -> str:
         else:
             label_link += convert_id(label)
     return label_link
->>>>>>> c4aeab0 ("init yeahbadou")
 
 
 def contains_label(labels: pd.Series, label: str) -> pd.Series:
     """
-<<<<<<< HEAD
     Créez une fonction qui prend une pandas Series de chaînes de charactères où chaque chaîne de charactères est formatée comme ci-dessus
     (c'est-à-dire "|" sépare les noms d'étiquettes comme "Music|Skateboard|Speech") et renvoie une pandas Series avec juste
     les valeurs qui incluent `label`.
@@ -89,26 +66,11 @@ def contains_label(labels: pd.Series, label: str) -> pd.Series:
     la fonction devrait retourner
     "Music|Skateboard|Speech"
     "Music|Piano"
-=======
-    Créez une fonction qui prend une pandas Series de chaînes de charactères où
-    chaque chaîne de charactères est formatée comme ci-dessus (c'est-à-dire "|"
-    sépare les noms d'étiquettes comme "Music|Skateboard|Speech") et renvoie une
-    pandas Series avec juste les valeurs qui incluent `label`.
-
-    Par exemple, étant donné le label "Music" et la série suivante :
-    "Music|Skateboard|Speech" "Voice|Speech" "Music|Piano"
-
-    la fonction devrait retourner "Music|Skateboard|Speech" "Music|Piano"
->>>>>>> c4aeab0 ("init yeahbadou")
 
     ATTENTION: Ce n'est pas suffisant de juste utiliser .contains
     """
-    # TODO
-<<<<<<< HEAD
-    pass
-=======
-    return labels[labels.str.contains(label, regex=False)].reset_index(drop=True)
->>>>>>> c4aeab0 ("init yeahbadou")
+    # BUG: Found 5708 rows, correct amount is 5695
+    return labels[labels.str.contains(label, regex=False, case = True)].reset_index(drop=True)
 
 
 def get_conditional_proportion(labels: pd.Series, label_1: str, label_2: str) -> float:
@@ -119,15 +81,10 @@ def get_conditional_proportion(labels: pd.Series, label_1: str, label_2: str) ->
     Par exemple, supposons que la pandas Series comporte 1 000 valeurs, dont 120 ont label_1. Si 30 des 120
     ont label_2, votre fonction doit renvoyer 0,25.
     """
-<<<<<<< HEAD
-    # TODO
-    pass
-=======
     full = contains_label(labels, label_1)
     full_size = full.size
     small_size = contains_label(full, label_2).size
     return small_size / full_size
->>>>>>> c4aeab0 ("init yeahbadou")
 
 
 if __name__ == "__main__":
@@ -135,18 +92,7 @@ if __name__ == "__main__":
     print(convert_id("/m/04rlf"))
     print(convert_ids("/m/04rlf,/m/06_fw,/m/09x0r"))
 
-<<<<<<< HEAD
-    series = pd.Series([
-        "Music|Skateboard|Speech",
-        "Voice|Speech",
-        "Music|Piano"
-    ])
-    print(contains_label(series, "Music"))
-    print(get_conditional_proportion(series, "Music", "Piano"))
-=======
     series = pd.Series(["Music|Skateboard|Speech", "Voice|Speech", "Music|Piano"])
     print(contains_label(series, "Music"))
     print(get_conditional_proportion(series, "Music", "Piano"))
-
     pd.read_csv("./data/audio_segments.csv")
->>>>>>> c4aeab0 ("init yeahbadou")
