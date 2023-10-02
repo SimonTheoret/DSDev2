05908dac69dbfe729e3ec0b56c76cf8ae1b068db
Simon Theoret <simonteoret@hotmail.com>
2023-10-02 08:45:10 -0400

    init q3

--- q2.py
+++ q2.py
@@ -1,5 +1,6 @@
 from yt_dlp import YoutubeDL
-
+from contextlib import redirect_stdout
+import io
 import ffmpeg
 import pandas as pd
 import numpy as np
@@ -28,7 +29,6 @@ def download_audio(YTID: str, path: str) -> None:
     """
     # TODO
     if exists(path+f"/{YTID}.mp3") == True:
-        print("Video already downloaded")
         return
 
     url = "https://www.youtube.com/watch?v=" + YTID
@@ -40,32 +40,39 @@ def download_audio(YTID: str, path: str) -> None:
                 "preferredcodec": "mp3",
             }
         ],
+        "quiet": True,
         "outtmpl": path+f"/{YTID}"
     }
+    trap = io.StringIO()
     with YoutubeDL(ydl_opts) as ydl:
-        try:
-            ydl.download(url)
-        except:
-            print(f"url: {url} cannot be downloaded")
+        with redirect_stdout(trap):
+            try:
+                ydl.download(url)
+            except:
+                pass
 
 
 def cut_audio(in_path: str, out_path: str, start: float, end: float) -> None:
     """
-    Créez une fonction qui coupe l'audio de in_path pour n'inclure que le segment de start à end et l'enregistre dans out_path.
+    Créez une fonction qui coupe l'audio de in_path pour n'inclure que le
+    segment de start à end et l'enregistre dans out_path.
 
-    ** Utilisez la bibliothèque ffmpeg : https://github.com/kkroening/ffmpeg-python
-    Arguments :
-    - in_path : Chemin du fichier audio à couper
-    - out_path : Chemin du fichier pour enregistrer l'audio coupé
-    - start : Indique le début de la séquence (en secondes)
-    - end : Indique la fin de la séquence (en secondes)
+    ** Utilisez la bibliothèque ffmpeg :
+    https://github.com/kkroening/ffmpeg-python Arguments : - in_path : Chemin du
+    fichier audio à couper - out_path : Chemin du fichier pour enregistrer
+    l'audio coupé - start : Indique le début de la séquence (en secondes) - end
+    : Indique la fin de la séquence (en secondes)
     """
     # TODO
-    ffmpeg.input(in_path, ss=start, t=end - start).output(out_path).run()
+    #
+    if exists(in_path) == True:
+        trap = io.StringIO()
+        with redirect_stdout(trap):
+            ffmpeg.input(in_path, ss=start, t=end - start).output(out_path, loglevel="quiet").run()
 
 
-if __name__ == "__main__":
-    id = "dIjUtfSSJ6Q"
-    path = "./output"
-    download_audio(id, path)
-    cut_audio(path+"/"+id+".mp3", "./output/out.mp3", 1, 3)
+# if __name__ == "__main__":
+#     id = "dIjUtfSSJ6Q"
+#     path = "./output"
+#     download_audio(id, path)
+#     cut_audio(path+"/"+id+".mp3", "./output/out.mp3", 1, 3)
--- q3.py
+++ q3.py
@@ -6,50 +6,89 @@ from q2 import download_audio, cut_audio
 from typing import List
 
 
-<<<<<<< HEAD
 def filter_df(csv_path: str, label: str) -> pd.DataFrame:
-=======
-def filter_df(csv_path: str, label: str) -> List[str]:
->>>>>>> c4aeab0 ("init yeahbadou")
     """
-    Écrivez une fonction qui prend le path vers le csv traité (dans la partie notebook de q1) et renvoie un df avec seulement les rangées qui contiennent l'étiquette `label`.
+    Écrivez une fonction qui prend le path vers le csv traité (dans la partie
+    notebook de q1) et renvoie un df avec seulement les rangées qui contiennent
+    l'étiquette `label`.
 
-    Par exemple:
-    get_ids("audio_segments_clean.csv", "Speech") ne doit renvoyer que les lignes où l'un des libellés est "Speech"
+    Par exemple: get_ids("audio_segments_clean.csv", "Speech") ne doit renvoyer
+    que les lignes où l'un des libellés est "Speech"
     """
     # TODO
-    pass
+    df = pd.read_csv(csv_path)
+    index = df["label_names"].map(lambda x: label in x)
+    return df[index]
 
 
 def data_pipeline(csv_path: str, label: str) -> None:
     """
-    En utilisant vos fonctions précédemment créées, écrivez une fonction qui prend un csv traité et pour chaque vidéo avec l'étiquette donnée:
-    1. Le télécharge à <label>_raw/<ID>.mp3
-    2. Le coupe au segment approprié
-    3. L'enregistre dans <label>_cut/<ID>.mp3
-    (n'oubliez pas de créer le dossier audio/ et le dossier label associé !).
+    En utilisant vos fonctions précédemment créées, écrivez une fonction qui
+    prend un csv traité et pour chaque vidéo avec l'étiquette donnée: 1. Le
+    télécharge à <label>_raw/<ID>.mp3 2. Le coupe au segment approprié 3.
+    L'enregistre dans <label>_cut/<ID>.mp3 (n'oubliez pas de créer le dossier
+    audio/ et le dossier label associé !).
 
-    Il est recommandé d'itérer sur les rangées de filter_df().
-    Utilisez tqdm pour suivre la progression du processus de téléchargement (https://tqdm.github.io/)
+    Il est recommandé d'itérer sur les rangées de filter_df().  Utilisez tqdm
+    pour suivre la progression du processus de téléchargement
+    (https://tqdm.github.io/)
 
-    Malheureusement, il est possible que certaines vidéos ne peuvent pas être téléchargées. Dans de tels cas, votre pipeline doit gérer l'échec en passant à la vidéo suivante avec l'étiquette.
+    Malheureusement, il est possible que certaines vidéos ne peuvent pas être
+    téléchargées. Dans de tels cas, votre pipeline doit gérer l'échec en passant
+    à la vidéo suivante avec l'étiquette.
     """
+
     # TODO
-    pass
+    def go(serie: pd.Series) -> None:
+        serie = serie.T
+        YTID = serie["# YTID"]
+        download_audio(serie["# YTID"], f"./{label}_raw")
+        start = serie["start_seconds"]
+        end = serie["end_seconds"]
+        in_path = f"./{label}_raw/{YTID}.mp3"
+        out_path = f"./{label}_cut/{YTID}.mp3"
+        out_dir = f"./{label}_cut/"
+        try:
+            os.mkdir(out_dir)
+        except:
+            pass
+        cut_audio(in_path, out_path, start, end)
+    tqdm.pandas(desc = "Advancement")
+    filter_df(csv_path, label).progress_apply(go, axis =1)
+
+    # for serie in tqdm(filter_df(csv_path, label).iterrows()):
+    #     YTID = serie["# YTID"]
+    #     download_audio(serie["# YTID"], f"./{label}_raw")
+    #     start = serie["start_seconds"]
+    #     end = serie["end_seconds"]
+    #     in_path = f"./{label}_raw/{YTID}.mp3"
+    #     out_path = f"./{label}_cut/{YTID}.mp3"
+    #     out_dir = f"./{label}_cut/"
+    #     try:
+    #         os.mkdir(out_dir)
+    #     except:
+    #         pass
+    #     cut_audio(in_path, out_path, start, end)
 
 
 def rename_files(path_cut: str, csv_path: str) -> None:
     """
-    Supposons que nous voulons maintenant renommer les fichiers que nous avons téléchargés dans `path_cut` pour inclure les heures de début et de fin ainsi que la longueur du segment. Alors que
-    cela aurait pu être fait dans la fonction data_pipeline(), supposons que nous avons oublié et que nous ne voulons pas tout télécharger à nouveau.
+    Supposons que nous voulons maintenant renommer les fichiers que nous avons
+    téléchargés dans `path_cut` pour inclure les heures de début et de fin ainsi
+    que la longueur du segment. Alors que cela aurait pu être fait dans la
+    fonction data_pipeline(), supposons que nous avons oublié et que nous ne
+    voulons pas tout télécharger à nouveau.
 
-    Écrivez une fonction qui, en utilisant regex (c'est-à-dire la bibliothèque `re`), renomme les fichiers existants de "<ID>.mp3" -> "<ID>_<start_seconds_int>_<end_seconds_int>_<length_int>.mp3"
-    dans path_cut. csv_path est le chemin vers le csv traité à partir de q1. `path_cut` est un chemin vers le dossier avec l'audio coupé.
+    Écrivez une fonction qui, en utilisant regex (c'est-à-dire la bibliothèque
+    `re`), renomme les fichiers existants de "<ID>.mp3" ->
+    "<ID>_<start_seconds_int>_<end_seconds_int>_<length_int>.mp3" dans path_cut.
+    csv_path est le chemin vers le csv traité à partir de q1. `path_cut` est un
+    chemin vers le dossier avec l'audio coupé.
 
-    Par exemple
-    "--BfvyPmVMo.mp3" -> "--BfvyPmVMo_20_30_10.mp3"
+    Par exemple "--BfvyPmVMo.mp3" -> "--BfvyPmVMo_20_30_10.mp3"
 
-    ## ATTENTION : supposez que l'YTID peut contenir des caractères spéciaux tels que '.' ou même '.mp3' ##
+    ## ATTENTION : supposez que l'YTID peut contenir des caractères spéciaux
+    ## tels que '.' ou même '.mp3' ##
     """
     # TODO
     pass
